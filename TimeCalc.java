54ce6267c8ff278be90b735823a4363e3aa9009b
kyle beliveau <kyle.beliveau@techhigh.us>
2014-10-03 09:24:53 -0400

    this is my almost working calculator

---
+++ TimeCalc.java
@@ -0,0 +1,10 @@
+public class TimeCalc{
+    public static void main(String[] args) {
+	int a = 5000;
+	int hr = a%3600/60;
+	int min = hr%60;
+	int sec = min%60;
+	system.out.println(hr + "," + min "," + sec );
+	}
+	
+}
