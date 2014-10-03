c1b46fa197ab8fa3f642a226f84579369d6ec196
kyle beliveau <kyle.beliveau@techhigh.us>
2014-10-03 09:36:41 -0400

    this is my working calc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,9 +1,9 @@
 public class TimeCalc{
     public static void main(String[] args) {
 	int a = 5000;
-	int hr = a%3600/60;
-	int min = hr%60;
-	int sec = min%60;
+	int hr = a/3600;
+	int min = a%3600/60;
+	int sec = a%60;
 	System.out.println(hr + "," + min + "," + sec );
 	}
 	
